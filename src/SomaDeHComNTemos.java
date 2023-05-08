8c1fa45bfbceb0212210942b00a0580e8e301d23
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-05-08 16:26:21 -0300

    Subindo Codigo

---
+++ src/SomaDeHComNTemos.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SomaDeHComNTemos {
+    public static void main(String[] Args) {
+
+        double h = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um valor: ");
+        double n = sc.nextDouble();
+
+        for (int i = 1; i <= n; i++) {
+
+            h = h + (1.0 / i);
+        }
+        System.out.print(Math.round(h));
+    }
+}
